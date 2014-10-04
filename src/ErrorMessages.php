13b200d6a2b9aa4047ade971407ddf226334f401
Francesco Zaffaroni <fraz.93>
2014-10-04 10:30:16 +0200

    Added DateRule message

--- src/ErrorMessages.php
+++ src/ErrorMessages.php
@@ -5,5 +5,6 @@ return [
     'email' => 'The :attribute field must be a valid email.',
     'numeric' => 'The :attribute field must be numeric.',
     'min' => 'The :attribute field must be at least :param characters long.',
-    'max' => 'The :attribute field must be maximum :param characters long.'
+    'max' => 'The :attribute field must be maximum :param characters long.',
+    'date' => 'The :attribute field must be a valid date in the :param format.'
 ];
